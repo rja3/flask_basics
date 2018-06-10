f247c850f45aecbcd7e3ed4d5485942c229c26ca
Richard Aplin <aplin@Richards-MBP.lan>
2018-06-10 21:03:16 +0100

    production config

--- book_catalog/config/prod.py
+++ book_catalog/config/prod.py
@@ -1,4 +1,4 @@
 DEBUG=False
 SECRET_KEY = 'secret-key'
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:dog-bark3@localhost:5433/catalog_db'
+SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 SQLALCHEMY_TRACK_MODIFICATIONS = False
