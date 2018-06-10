aa3109d4174afa700123a06f5e8f239d0e5f5ed0
Richard Aplin <aplin@Richards-MBP.lan>
2018-06-10 21:01:37 +0100

    production config

---
+++ book_catalog/config/prod.py
@@ -0,0 +1,4 @@
+DEBUG=False
+SECRET_KEY = 'secret-key'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:dog-bark3@localhost:5433/catalog_db'
+SQLALCHEMY_TRACK_MODIFICATIONS = False
--- book_catalog/run.py
+++ book_catalog/run.py
@@ -2,11 +2,10 @@ from booky import create_app, db
 from booky.auth.models import User
 
 
-if __name__ == '__main__':
-    flask_app = create_app('dev')
 
-    with flask_app.app_context():
-        db.create_all()
+flask_app = create_app('prod')
 
+with flask_app.app_context():
+    db.create_all()
     flask_app.run()
 
